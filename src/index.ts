e5060fb6b998ebb60f993cafb06fadbbbc79d8b0
Thanatat Tamtan <acoshift@gmail.com>
2016-01-28 23:25:37 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -3,47 +3,48 @@ import * as path from 'path'
 import * as url from 'url'
 import * as fs from 'fs'
 import * as _ from 'lodash'
+import { Observable, Observer } from 'rxjs'
 import appConfig from './config'
 import PageConfig from './lib/page-config'
 import RouteConfig from './lib/route-config'
+import FileConfig from './lib/file-config'
 
-/* FileConfig
- * Operator
- * 1. Read file configs from db
- * 2. Write data to file in pagesDir
- * 3. Remove writed file configs from db
-*/
-interface FileConfig {
-  _id: string
-  pageId: string
-  name: string
-  path: string // reject reletive path
-  data: string // base64
-}
+let configs: PageConfig[] = []
+
+reloadPageConfig()
+operateFileConfig()
+
+let patch: Observer<{}>
+const observable = (<Observable<{}>> Observable.create((observer: Observer<{}>) => { patch = observer })).share()
+
+observable
+  .filter(x => x === 1)
+  .debounceTime(appConfig.debounce)
+  .subscribe(() => {
+    reloadPageConfig()
+  })
+
+observable
+  .filter(x => x === 2)
+  .debounceTime(appConfig.debounce)
+  .subscribe(() => {
+    operateFileConfig()
+  })
+
+setInterval(() => { patch.next(1); patch.next(2) }, appConfig.interval)
 
 const app = express()
 app.disable('x-powered-by')
 
-let configs: PageConfig[] = []
+app.patch('/route', (req, res) => {
+  patch.next(1)
+  res.sendStatus(200)
+})
 
-function reloadPageConfig (): void {
-  PageConfig.load((err, result) => {
-    if (err || !result) return // skip error
-    configs = result
-    RouteConfig.operate(result)
-  })
-};
-
-setInterval(reloadPageConfig, appConfig.interval)
-reloadPageConfig()
-
-function operateFileConfig (): void {
-  let pageConfigs = configs
-  if (_.isEmpty(pageConfigs)) return
-
-}
-
-setInterval(operateFileConfig, appConfig.operateFileInterval)
+app.patch('/file', (req, res) => {
+  patch.next(2)
+  res.sendStatus(200)
+})
 
 app.use((req, res, next) => {
   // find PageConfig from request
@@ -53,7 +54,7 @@ app.use((req, res, next) => {
   let [ name ] = q
 
   let c = _.find(configs, x => x.name === name)
-  if (!c) {
+  if (_.isNil(c)) {
     // no config => local
     req.url = `/local${req.url}`
     return next()
@@ -72,7 +73,19 @@ app.use((req, res, next) => {
 })
 
 app.use(express.static(path.join(__dirname, appConfig.pagesDir)))
-
 app.use((req, res) => { res.sendStatus(404) })
-
 app.listen(appConfig.port)
+
+function reloadPageConfig (): void {
+  PageConfig.load((err, result) => {
+    if (err || !result) return // skip error
+    configs = result
+    RouteConfig.operate(result)
+  })
+}
+
+function operateFileConfig (): void {
+  let pageConfigs = configs
+  if (_.isEmpty(pageConfigs)) return
+  FileConfig.operate(pageConfigs)
+}
---
+++ src/lib/file-config.ts
@@ -0,0 +1,141 @@
+import * as http from 'http'
+import * as https from 'https'
+import * as path from 'path'
+import * as fs from 'fs'
+import * as _ from 'lodash'
+import config from '../config'
+import PageConfig from './page-config'
+
+const _http = config.db.protocol === 'https' ? https : http
+
+function decode (base64: string): string {
+  return base64 ? new Buffer(base64, 'base64').toString() : null
+}
+
+interface FileConfig {
+  _id: string
+  pageId: string
+  name: string
+  path: string // reject reletive path
+  data: string // base64
+  op: string
+}
+
+class FileConfig {
+  pageName: string
+
+  constructor (obj: FileConfig) {
+    this._id = obj._id || ''
+    this.pageId = obj.pageId || ''
+    this.name = obj.name || ''
+    this.path = obj.path || ''
+    this.data = obj.data || ''
+    this.op = obj.op || ''
+  }
+
+  get valid (): boolean {
+    return !!this._id && !!this.pageId && !!this.name && !!this.path && !!this.op && !!this.pageName
+  }
+
+  error (): void {
+    let opt: http.RequestOptions = {
+      host: config.db.host,
+      port: config.db.port,
+      path: '/' + config.db.ns,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/nepq',
+        'Authorization': 'Bearer ' + config.db.token
+      }
+    }
+    let req = _http.request(opt)
+    req.on('error', () => {})
+    req.end(`update files("${this._id}",{op:"error"}){}`)
+  }
+
+  delete (): void {
+    if (!path.isAbsolute(this.path)) return this.error()
+
+    fs.unlink(path.join(config.pagesDir, this.pageName, this.path, this.name), err => {
+      this.data = undefined
+      if (err) return this.error()
+
+      let opt: http.RequestOptions = {
+        host: config.db.host,
+        port: config.db.port,
+        path: '/' + config.db.ns,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/nepq',
+          'Authorization': 'Bearer ' + config.db.token
+        }
+      }
+      let req = _http.request(opt)
+      req.on('error', () => {})
+      req.end(`delete files("${this._id}"){}`)
+    })
+  }
+
+  update (): void {
+    this.data = decode(this.data)
+    if (!path.isAbsolute(this.path) || _.isNull(this.data)) return this.error()
+
+    fs.writeFile(path.join(config.pagesDir, this.pageName, this.path, this.name), this.data, err => {
+      this.data = undefined
+      if (err) return this.error()
+
+      let opt: http.RequestOptions = {
+        host: config.db.host,
+        port: config.db.port,
+        path: '/' + config.db.ns,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/nepq',
+          'Authorization': 'Bearer ' + config.db.token
+        }
+      }
+      let req = _http.request(opt)
+      req.on('error', () => {})
+      req.end(`update files("${this._id}",{},["data", "op"]){}`)
+    })
+  }
+
+  static operate (pages: PageConfig[]): void {
+    let opt: http.RequestOptions = {
+      host: config.db.host,
+      port: config.db.port,
+      path: '/' + config.db.ns,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/nepq',
+        'Authorization': 'Bearer ' + config.db.token
+      }
+    }
+    let req = _http.request(opt, res => {
+      let data = []
+      res
+        .on('data', d => {
+          data.push(d)
+        })
+        .on('end', () => {
+          if (res.statusCode !== 200) return
+          try {
+            ;_(<FileConfig[]> JSON.parse(Buffer.concat(data).toString('utf8')))
+              .map(x => new FileConfig(x))
+              .forEach(x => {
+                let p = _.find(pages, p => p._id === x.pageId)
+                if (p) x.pageName = p.name
+                if (!x.valid) return x.error()
+                if (x.op === 'update') return x.update()
+                if (x.op === 'delete') return x.delete()
+                x.error()
+              })
+          } catch (e) {}
+        })
+    })
+    req.on('error', () => {})
+    req.end('query files(op:{$in:["update","delete"]}){_id,pageId,name,path,data,op}')
+  }
+}
+
+export default FileConfig
